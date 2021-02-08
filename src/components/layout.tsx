78f7ae4da42d03796ae3c7b977ccdca20c69fdfc
Balance8 <mslocum819@gmail.com>
2021-02-08 13:40:30 -0800

    set up auth flow

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,19 +1,20 @@
 import { FunctionComponent, ReactNode } from "react";
 import Link from "next/link";
-// import { useAuth } from "src/auth/useAuth";
+import { useAuth } from "src/auth/useAuth";
 
 interface IProps {
   main: ReactNode;
 }
 
 const Layout: FunctionComponent<IProps> = ({ main }) => {
-  const authenticated = false;
-  const logout = () => null;
+  const { logout, authenticated } = useAuth();
+  // const authenticated = false;
+  // const logout = () => null;
 
   return (
-    <div className="bg-gray-900 max-w-screen-2xl mx-auto text-white">
+    <div className="mx-auto text-white bg-gray-900 max-w-screen-2xl">
       <nav className="bg-gray-800" style={{ height: "64px" }}>
-        <div className="px-6 bg-gray-800 flex items-center justify-between h-16">
+        <div className="flex items-center justify-between h-16 px-6 bg-gray-800">
           <Link href="/">
             <a>
               <img
