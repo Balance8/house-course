fcd770ebf55104f62664d39b15df05663f810cca
Balance8 <mslocum819@gmail.com>
2021-02-11 16:41:56 -0800

    16

--- pages/houses/[id]/index.tsx
+++ pages/houses/[id]/index.tsx
@@ -1,14 +1,55 @@
-// import { useRouter } from "next/router";
-// import { Image } from "cloudinary-react";
-// import { useQuery, gql } from "@apollo/client";
-// import Layout from "src/components/layout";
+import { useRouter } from "next/router";
+import { Image } from "cloudinary-react";
+import { useQuery, gql } from "@apollo/client";
+import Layout from "src/components/layout";
 // import HouseNav from "src/components/houseNav";
 // import SingleMap from "src/components/singleMap";
-// import {
-//   ShowHouseQuery,
-//   ShowHouseQueryVariables,
-// } from "src/generated/ShowHouseQuery";
+import {
+  ShowHouseQuery,
+  ShowHouseQueryVariables,
+} from "src/generated/ShowHouseQuery";
+
+const SHOW_HOUSE_QUERY = gql`
+  query ShowHouseQuery($id: String!) {
+    house(id: $id) {
+      id
+      userId
+      address
+      publicId
+      bedrooms
+      latitude
+      longitude
+    }
+  }
+`;
 
 export default function ShowHouse() {
-  return <div>ShowHouse</div>;
+  const {
+    query: { id },
+  } = useRouter();
+
+  if (!id) return null;
+  return <HouseData id={id as string} />;
+}
+
+function HouseData({ id }: { id: string }) {
+  const { data, loading } = useQuery<ShowHouseQuery, ShowHouseQueryVariables>(
+    SHOW_HOUSE_QUERY,
+    { variables: { id } }
+  );
+  if (loading || !data) return <Layout main={<div>Loading...</div>} />;
+  if (!data.house)
+    return <Layout main={<div>Unable to load house {id}</div>} />;
+
+  const { house } = data;
+
+  return (
+    <Layout
+      main={
+        <div>
+          <pre>{JSON.stringify(house, null, 2)}</pre>
+        </div>
+      }
+    />
+  );
 }
--- src/schema/house.ts
+++ src/schema/house.ts
@@ -15,6 +15,7 @@ import {
 import { Min, Max } from "class-validator";
 import { getBoundsOfDistance } from "geolib";
 import { Context, AuthorizedContext } from "./context";
+import { parse } from "graphql";
 
 @InputType()
 class CoordinatesInput {
@@ -76,6 +77,11 @@ class House {
 
 @Resolver()
 export class HouseResolver {
+  @Query((_returns) => House, { nullable: true })
+  async house(@Arg("id") id: string, @Ctx() ctx: Context) {
+    return ctx.prisma.house.findOne({ where: { id: parseInt(id, 10) } });
+  }
+
   @Authorized()
   @Mutation((_returns) => House, { nullable: true })
   async createHouse(
