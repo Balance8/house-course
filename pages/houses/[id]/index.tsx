65b7e45a10a5c731d41f6d84ad072ae9f3809cec
Balance8 <mslocum819@gmail.com>
2021-02-12 12:26:58 -0800

    updateHouse

--- pages/houses/[id]/index.tsx
+++ pages/houses/[id]/index.tsx
@@ -2,7 +2,7 @@ import { useRouter } from "next/router";
 import { Image } from "cloudinary-react";
 import { useQuery, gql } from "@apollo/client";
 import Layout from "src/components/layout";
-// import HouseNav from "src/components/houseNav";
+import HouseNav from "src/components/houseNav";
 import SingleMap from "src/components/singleMap";
 import {
   ShowHouseQuery,
@@ -53,6 +53,8 @@ function HouseData({ id }: { id: string }) {
       main={
         <div className="sm:block md:flex">
           <div className="p-4 sm:w-full md:w-1/2">
+            <HouseNav house={house} />
+
             <h1 className="my-2 text-3xl">{house.address}</h1>
 
             <Image
--- src/components/houseForm.tsx
+++ src/components/houseForm.tsx
@@ -32,6 +32,20 @@ const CREATE_HOUSE_MUTATION = gql`
   }
 `;
 
+const UPDATE_HOUSE_MUTATION = gql`
+  mutation UpdateHouseMutation($id: String!, $input: HouseInput!) {
+    updateHouse(id: $id, input: $input) {
+      id
+      image
+      publicId
+      latitude
+      longitude
+      bedrooms
+      address
+    }
+  }
+`;
+
 interface IUploadImageResponse {
   secure_url: string;
 }
@@ -63,7 +77,19 @@ interface IFormData {
   image: FileList;
 }
 
-interface IProps {}
+interface IHouse {
+  id: string;
+  address: string;
+  latitude: number;
+  longitude: number;
+  bedrooms: number;
+  image: string;
+  publicId: string;
+}
+
+interface IProps {
+  house?: IHouse;
+}
 
 export default function HouseForm({}: IProps) {
   const router = useRouter();
